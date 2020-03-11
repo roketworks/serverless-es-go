41ac05d83344cef732527ef288366457b36b84e6
Mike McFarland <roketworks@users.noreply.github.com>
2020-03-11 13:15:25 +0000

    Add timestamp to checkpoint

--- eventstore/checkpoint.go
+++ eventstore/checkpoint.go
@@ -10,7 +10,7 @@ type CheckpointConfig struct {
 	ProjectionName   string
 }
 
-func SaveCheckpoint(cfg *CheckpointConfig, position int) error {
+func SaveCheckpoint(cfg *CheckpointConfig, position int, timestamp int64) error {
 	db, err := sql.Open("postgres", cfg.ConnectionString)
 	if err != nil {
 		return err
@@ -21,7 +21,7 @@ func SaveCheckpoint(cfg *CheckpointConfig, position int) error {
 	}
 
 	db.Begin()
-	if _, err = db.Exec(update, cfg.ProjectionName, position); err != nil {
+	if _, err = db.Exec(update, cfg.ProjectionName, position, timestamp); err != nil {
 		db.Close()
 		return err
 	}
@@ -34,15 +34,17 @@ const schema = `
 	CREATE TABLE IF NOT EXISTS checkpoints
 	(
 		name        varchar(50) unique,
-		position    int
+		position    int,
+		timestamp 	bigint
 	);
 `
 
 const update = `
-	INSERT INTO checkpoints (name, position)
-	VALUES ($1, $2)
+	INSERT INTO checkpoints (name, position, timestamp)
+	VALUES ($1, $2, $3)
 	ON CONFLICT(name)
 	DO
 	  UPDATE SET
-		position = $2
+		position = $2,
+		timestamp = $3
 `
--- eventstore/checkpoint_test.go
+++ eventstore/checkpoint_test.go
@@ -14,21 +14,24 @@ var connectionString = test.Configuration.Postgres.ConnectionString
 
 func TestSaveNewCheckpoint(t *testing.T) {
 	cfg := CheckpointConfig{ConnectionString: connectionString, ProjectionName: "test-projection"}
-	err := SaveCheckpoint(&cfg, 1)
+	timestamp := getTimestamp()
+	err := SaveCheckpoint(&cfg, 1, timestamp)
 	assert.Nil(t, err)
-	verifyCheckpoint(t, "test-projection", 1)
+	verifyCheckpoint(t, "test-projection", 1, timestamp)
 }
 
-func verifyCheckpoint(t *testing.T, name string, position int) {
+func verifyCheckpoint(t *testing.T, name string, position int, timestamp int64) {
 	db, err := sql.Open("postgres", connectionString)
 	assert.Nil(t, err)
 
 	var projectionName string
 	var checkpointPosition int
+	var checkpointTimestamp int64
 
-	err = db.QueryRow("SELECT * FROM checkpoints WHERE name = $1", name).Scan(&projectionName, &checkpointPosition)
+	err = db.QueryRow("SELECT * FROM checkpoints WHERE name = $1", name).Scan(&projectionName, &checkpointPosition, &checkpointTimestamp)
 	assert.Nil(t, err)
 
 	assert.Equal(t, name, projectionName)
 	assert.Equal(t, position, checkpointPosition)
+	assert.Equal(t, timestamp, checkpointTimestamp)
 }

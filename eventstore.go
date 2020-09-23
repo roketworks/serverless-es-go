0f99a3acc5b3643238ca6dd68bae87f185e5e762
Mike McFarland <roketworks@users.noreply.github.com>
2020-09-23 14:33:16 +0100

    Update lint errors

--- eventstore.go
+++ eventstore.go
@@ -12,8 +12,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
-type DynamoDbEventStore struct {
-	Db         dynamodbiface.DynamoDBAPI
+type DynamoDBEventStore struct {
+	DB         dynamodbiface.DynamoDBAPI
 	EventTable string
 }
 
@@ -23,7 +23,7 @@ const (
 )
 
 type Event struct {
-	StreamId        string `dynamodbav:"streamId"`
+	StreamID        string `dynamodbav:"streamId"`
 	Version         int    `dynamodbav:"version"`
 	CommittedAt     int64  `dynamodbav:"committedAt"`
 	MessagePosition int64  `dynamodbav:"position"`
@@ -32,20 +32,20 @@ type Event struct {
 }
 
 // NewEventStore return an eventstore
-func NewEventStore(dynamodb dynamodbiface.DynamoDBAPI, table string) *DynamoDbEventStore {
-	return &DynamoDbEventStore{
-		Db:         dynamodb,
+func NewEventStore(dynamodb dynamodbiface.DynamoDBAPI, table string) *DynamoDBEventStore {
+	return &DynamoDBEventStore{
+		DB:         dynamodb,
 		EventTable: table,
 	}
 }
 
 // ReadStreamEventsForward reads from the specified stream id starting at specified index and reads forward by the count
 // Query is inclusive of start position
-func (es *DynamoDbEventStore) ReadStreamEventsForward(streamId string, start int64, count int64) ([]Event, error) {
-	keyCondition := "streamId = :s"
+func (es *DynamoDBEventStore) ReadStreamEventsForward(streamID string, start int64, count int64) ([]Event, error) {
+	keyCondition := "streamID = :s"
 	expressionValues := map[string]*dynamodb.AttributeValue{
 		":s": {
-			S: aws.String(streamId),
+			S: aws.String(streamID),
 		},
 	}
 
@@ -76,13 +76,13 @@ func (es *DynamoDbEventStore) ReadStreamEventsForward(streamId string, start int
 	return res, nil
 }
 
-// ReadStreamEventsBackward reads from the specified stream id starting at specified index and reads backward by the count
+// ReadStreamEventsBackward reads from the specified stream starting at specified index and reads backward by the count
 // Query is inclusive of start position
-func (es *DynamoDbEventStore) ReadStreamEventsBackward(streamId string, start int64, count int64) ([]Event, error) {
-	keyCondition := "streamId = :s"
+func (es *DynamoDBEventStore) ReadStreamEventsBackward(streamID string, start int64, count int64) ([]Event, error) {
+	keyCondition := "streamID = :s"
 	expressionValues := map[string]*dynamodb.AttributeValue{
 		":s": {
-			S: aws.String(streamId),
+			S: aws.String(streamID),
 		},
 	}
 
@@ -115,7 +115,7 @@ func (es *DynamoDbEventStore) ReadStreamEventsBackward(streamId string, start in
 
 // ReadAllEventsForward reads from events across all streams starting at specified index and reads forward by the count
 // Query is inclusive of start position
-func (es *DynamoDbEventStore) ReadAllEventsForward(position int64, count int64) ([]Event, error) {
+func (es *DynamoDBEventStore) ReadAllEventsForward(position int64, count int64) ([]Event, error) {
 	keyCondition := "active = :a"
 	var expressionNames map[string]*string = nil
 	expressionValues := map[string]*dynamodb.AttributeValue{
@@ -147,16 +147,17 @@ func (es *DynamoDbEventStore) ReadAllEventsForward(position int64, count int64) 
 		input.Limit = aws.Int64(count)
 	}
 
-	if res, err := queryEvents(es, input); err != nil {
+	res, err := queryEvents(es, input)
+	if err != nil {
 		return nil, err
-	} else {
-		return res, nil
 	}
+
+	return res, nil
 }
 
-// ReadAllEventsBackward reads from events across all streams starting at specified index and reads backward by the count
+// ReadAllEventsBackward reads from events from all streams starting at specified index and reads backward by the count
 // Query is inclusive of start position
-func (es *DynamoDbEventStore) ReadAllEventsBackward(position int64, count int64) ([]Event, error) {
+func (es *DynamoDBEventStore) ReadAllEventsBackward(position int64, count int64) ([]Event, error) {
 	keyCondition := "active = :a"
 	var expressionNames map[string]*string = nil
 	expressionValues := map[string]*dynamodb.AttributeValue{
@@ -188,15 +189,16 @@ func (es *DynamoDbEventStore) ReadAllEventsBackward(position int64, count int64)
 		input.Limit = aws.Int64(count)
 	}
 
-	if res, err := queryEvents(es, input); err != nil {
+	res, err := queryEvents(es, input)
+	if err != nil {
 		return nil, err
-	} else {
-		return res, nil
 	}
+
+	return res, nil
 }
 
 // Save a new event to a specified stream. Returns the global position of the message.
-func (es *DynamoDbEventStore) Save(streamId string, expectedVersion int, eventType string, event []byte) (int64, error) {
+func (es *DynamoDBEventStore) Save(streamID string, version int, eventType string, event []byte) (int64, error) {
 	commitTime := strconv.FormatInt(getTimestamp(), 10)
 	lastEvent, err := es.ReadAllEventsBackward(PositionEnd, 1)
 	if err != nil {
@@ -204,7 +206,7 @@ func (es *DynamoDbEventStore) Save(streamId string, expectedVersion int, eventTy
 	}
 
 	var lastPosition int64
-	if lastEvent == nil || len(lastEvent) == 0 {
+	if len(lastEvent) == 0 {
 		lastPosition = 0
 	} else {
 		lastPosition = lastEvent[0].MessagePosition
@@ -214,14 +216,14 @@ func (es *DynamoDbEventStore) Save(streamId string, expectedVersion int, eventTy
 
 	input := &dynamodb.PutItemInput{
 		Item: map[string]*dynamodb.AttributeValue{
-			"streamId": {
-				S: aws.String(streamId),
+			"streamID": {
+				S: aws.String(streamID),
 			},
 			"committedAt": {
 				N: aws.String(commitTime),
 			},
 			"version": {
-				N: aws.String(strconv.Itoa(expectedVersion)),
+				N: aws.String(strconv.Itoa(version)),
 			},
 			"active": {
 				N: aws.String("1"),
@@ -244,12 +246,12 @@ func (es *DynamoDbEventStore) Save(streamId string, expectedVersion int, eventTy
 		TableName:           aws.String(es.EventTable),
 	}
 
-	_, err = es.Db.PutItem(input)
+	_, err = es.DB.PutItem(input)
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
 			case dynamodb.ErrCodeConditionalCheckFailedException:
-				return -1, errors.New("A commit already exists with the specified version")
+				return -1, errors.New("a commit already exists with the specified version")
 			default:
 				return -1, aerr
 			}
@@ -260,10 +262,10 @@ func (es *DynamoDbEventStore) Save(streamId string, expectedVersion int, eventTy
 	return position, nil
 }
 
-func queryEvents(es *DynamoDbEventStore, queryInput *dynamodb.QueryInput) ([]Event, error) {
+func queryEvents(es *DynamoDBEventStore, queryInput *dynamodb.QueryInput) ([]Event, error) {
 	queryFunc := func(lastKey map[string]*dynamodb.AttributeValue) ([]Event, map[string]*dynamodb.AttributeValue, error) {
 		queryInput.ExclusiveStartKey = lastKey
-		result, err := es.Db.Query(queryInput)
+		result, err := es.DB.Query(queryInput)
 		if err != nil {
 			return nil, nil, err
 		}
@@ -285,9 +287,7 @@ func queryEvents(es *DynamoDbEventStore, queryInput *dynamodb.QueryInput) ([]Eve
 	}
 
 	for {
-		for _, r := range results {
-			res = append(res, r)
-		}
+		res = append(res, results...)
 
 		if lastKey == nil || (queryInput.Limit != nil && int64(len(res)) >= *queryInput.Limit) {
 			break
--- eventstore_test.go
+++ eventstore_test.go
@@ -2,18 +2,19 @@ package esgo
 
 import (
 	"fmt"
+	"testing"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
-var es *DynamoDbEventStore
+var es *DynamoDBEventStore
 
-func init() {
+func TestMain(m *testing.M) {
 	config := aws.NewConfig()
 	config.Endpoint = aws.String("http://localhost:4566")
 	config.Region = aws.String("eu-west-1")
@@ -25,26 +26,26 @@ func init() {
 }
 
 func TestSaveShouldSaveToNewStream(t *testing.T) {
-	streamId := fmt.Sprintf("stream-%v", uuid.New().String())
+	streamID := fmt.Sprintf("stream-%v", uuid.New().String())
 
-	_, err := es.Save(streamId, 1, "event-type", []byte(eventData))
+	_, err := es.Save(streamID, 1, "event-type", []byte(eventData))
 	assert.Nil(t, err)
 }
 
 func TestDynamoDbEventStore_ReadStreamEventsForward_FromStart_ToEnd(t *testing.T) {
-	streamId := fmt.Sprintf("stream-%v", uuid.New().String())
+	streamID := fmt.Sprintf("stream-%v", uuid.New().String())
 
 	preCommitTime := getTimestamp()
-	saveTestEvents(streamId, 4, 1)
+	saveTestEvents(streamID, 4)
 	postCommit := getTimestamp()
 
-	events, err := es.ReadStreamEventsForward(streamId, PositionStart, PositionEnd)
+	events, err := es.ReadStreamEventsForward(streamID, PositionStart, PositionEnd)
 
 	assert.Nil(t, err)
 	assert.Equal(t, 4, len(events))
 
 	for i, event := range events {
-		assert.Equal(t, streamId, event.StreamId)
+		assert.Equal(t, streamID, event.StreamID)
 		assert.Equal(t, i+1, event.Version)
 		assert.GreaterOrEqual(t, event.CommittedAt, preCommitTime)
 		assert.LessOrEqual(t, event.CommittedAt, postCommit)
@@ -53,19 +54,19 @@ func TestDynamoDbEventStore_ReadStreamEventsForward_FromStart_ToEnd(t *testing.T
 }
 
 func TestDynamoDbEventStore_ReadStreamEventsForward_FromPositionInStream(t *testing.T) {
-	streamId := fmt.Sprintf("stream-%v", uuid.New().String())
+	streamID := fmt.Sprintf("stream-%v", uuid.New().String())
 
 	preCommitTime := getTimestamp()
-	saveTestEvents(streamId, 4, 1)
+	saveTestEvents(streamID, 4)
 	postCommit := getTimestamp()
 
-	events, err := es.ReadStreamEventsForward(streamId, 3, 2)
+	events, err := es.ReadStreamEventsForward(streamID, 3, 2)
 
 	assert.Nil(t, err)
 	assert.Equal(t, 2, len(events))
 
 	for i, event := range events {
-		assert.Equal(t, streamId, event.StreamId)
+		assert.Equal(t, streamID, event.StreamID)
 		assert.Equal(t, i+3, event.Version)
 		assert.GreaterOrEqual(t, event.CommittedAt, preCommitTime)
 		assert.LessOrEqual(t, event.CommittedAt, postCommit)
@@ -74,17 +75,17 @@ func TestDynamoDbEventStore_ReadStreamEventsForward_FromPositionInStream(t *test
 }
 
 func TestDynamoDbEventStore_ReadStreamEventsBackward_FromEnd(t *testing.T) {
-	streamId := fmt.Sprintf("stream-%v", uuid.New().String())
+	streamID := fmt.Sprintf("stream-%v", uuid.New().String())
 
 	preCommitTime := getTimestamp()
-	saveTestEvents(streamId, 4, 1)
+	saveTestEvents(streamID, 4)
 	postCommit := getTimestamp()
 
-	events, err := es.ReadStreamEventsBackward(streamId, PositionEnd, PositionStart)
+	events, err := es.ReadStreamEventsBackward(streamID, PositionEnd, PositionStart)
 	assert.Nil(t, err)
 	assert.Equal(t, 4, len(events))
 	for i, event := range events {
-		assert.Equal(t, streamId, event.StreamId)
+		assert.Equal(t, streamID, event.StreamID)
 		assert.Equal(t, 4-i, event.Version)
 		assert.GreaterOrEqual(t, event.CommittedAt, preCommitTime)
 		assert.LessOrEqual(t, event.CommittedAt, postCommit)
@@ -93,17 +94,17 @@ func TestDynamoDbEventStore_ReadStreamEventsBackward_FromEnd(t *testing.T) {
 }
 
 func TestDynamoDbEventStore_ReadStreamEventsBackward_FromPosition(t *testing.T) {
-	streamId := fmt.Sprintf("stream-%v", uuid.New().String())
+	streamID := fmt.Sprintf("stream-%v", uuid.New().String())
 
 	preCommitTime := getTimestamp()
-	saveTestEvents(streamId, 4, 1)
+	saveTestEvents(streamID, 4)
 	postCommit := getTimestamp()
 
-	events, err := es.ReadStreamEventsBackward(streamId, 3, 2)
+	events, err := es.ReadStreamEventsBackward(streamID, 3, 2)
 	assert.Nil(t, err)
 	assert.Equal(t, 2, len(events))
 	for i, event := range events {
-		assert.Equal(t, streamId, event.StreamId)
+		assert.Equal(t, streamID, event.StreamID)
 		assert.Equal(t, 3-i, event.Version)
 		assert.GreaterOrEqual(t, event.CommittedAt, preCommitTime)
 		assert.LessOrEqual(t, event.CommittedAt, postCommit)
@@ -112,12 +113,12 @@ func TestDynamoDbEventStore_ReadStreamEventsBackward_FromPosition(t *testing.T) 
 }
 
 func TestDynamoDbEventStore_ReadAllEventsForward_FromStart_ToEnd(t *testing.T) {
-	streamId := fmt.Sprintf("stream-%v", uuid.New().String())
+	streamID := fmt.Sprintf("stream-%v", uuid.New().String())
 	lastEvent, err := es.ReadAllEventsBackward(PositionEnd, 1)
 	assert.Nil(t, err)
 
 	initialPosition := lastEvent[0].MessagePosition
-	saveTestEvents(streamId, 10, 1)
+	saveTestEvents(streamID, 10)
 
 	events, err := es.ReadAllEventsForward(PositionStart, PositionEnd)
 	assert.Nil(t, err)
@@ -129,12 +130,12 @@ func TestDynamoDbEventStore_ReadAllEventsForward_FromStart_ToEnd(t *testing.T) {
 }
 
 func TestDynamoDbEventStore_ReadAllEventsForward_FromPosition(t *testing.T) {
-	streamId := fmt.Sprintf("stream-%v", uuid.New().String())
+	streamID := fmt.Sprintf("stream-%v", uuid.New().String())
 	lastEvent, err := es.ReadAllEventsBackward(PositionEnd, 1)
 	assert.Nil(t, err)
 
 	initialPosition := lastEvent[0].MessagePosition
-	saveTestEvents(streamId, 10, 1)
+	saveTestEvents(streamID, 10)
 
 	events, err := es.ReadAllEventsForward(initialPosition+1, 5)
 	assert.Nil(t, err)
@@ -146,12 +147,12 @@ func TestDynamoDbEventStore_ReadAllEventsForward_FromPosition(t *testing.T) {
 }
 
 func TestDynamoDbEventStore_ReadAllEventsBackward_FromStart_ToEnd(t *testing.T) {
-	streamId := fmt.Sprintf("stream-%v", uuid.New().String())
+	streamID := fmt.Sprintf("stream-%v", uuid.New().String())
 	lastEvent, err := es.ReadAllEventsBackward(PositionEnd, 1)
 	assert.Nil(t, err)
 
 	initialPosition := lastEvent[0].MessagePosition
-	saveTestEvents(streamId, 10, 1)
+	saveTestEvents(streamID, 10)
 
 	events, err := es.ReadAllEventsBackward(PositionEnd, PositionStart)
 	assert.Nil(t, err)
@@ -163,12 +164,12 @@ func TestDynamoDbEventStore_ReadAllEventsBackward_FromStart_ToEnd(t *testing.T) 
 }
 
 func TestDynamoDbEventStore_ReadAllEventsBackward_FromPosition(t *testing.T) {
-	streamId := fmt.Sprintf("stream-%v", uuid.New().String())
+	streamID := fmt.Sprintf("stream-%v", uuid.New().String())
 	lastEvent, err := es.ReadAllEventsBackward(PositionEnd, 1)
 	assert.Nil(t, err)
 
 	initialPosition := lastEvent[0].MessagePosition
-	saveTestEvents(streamId, 10, 1)
+	saveTestEvents(streamID, 10)
 
 	events, err := es.ReadAllEventsBackward(initialPosition+1, 5)
 	assert.Nil(t, err)
@@ -179,9 +180,9 @@ func TestDynamoDbEventStore_ReadAllEventsBackward_FromPosition(t *testing.T) {
 	}
 }
 
-func saveTestEvents(stream string, count int, start int) {
+func saveTestEvents(stream string, count int) {
 	for i := 0; i < count; i++ {
-		if _, err := es.Save(stream, start+i, "testevent", []byte(eventData)); err != nil {
+		if _, err := es.Save(stream, i+1, "testevent", []byte(eventData)); err != nil {
 			panic(err)
 		}
 	}
